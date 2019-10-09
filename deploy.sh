7995eedaac2757f65f118b37352c811f53d1000f
Gabriel Wu <qqbbnease1004@126.com>
2019-10-09 14:24:23 +0800

    chore: update travis script

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,9 @@ sed 's@YOUR GOOGLE MAP API KEY@'${GOOGLE_API_KEY}'@' -i .env
 yarn build && \
 cp now.json build && \
 cd build && \
-now --token $NOW_TOKEN && \
-now alias --token $NOW_TOKEN && \
-now remove map-example-CRA --safe -y --token $NOW_TOKEN && \
+now --prod --token $NOW_TOKEN && \
 cd ../../../packages/lib && \
 yarn styleguide:build && \
 cp now.json styleguide && \
 cd styleguide && \
-now --token $NOW_TOKEN && \
-now alias --token $NOW_TOKEN && \
-now remove react-google-map --safe -y --token $NOW_TOKEN
+now --prod --token $NOW_TOKEN

4b1bd18aa16c3ead50ef7e1371726abf482247dd
Gabriel Wu <qqbbnease1004@126.com>
2020-06-27 06:26:25 +0800

    chore: fix CI

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ sed 's@YOUR GOOGLE MAP API KEY@'${GOOGLE_API_KEY}'@' -i .env
 yarn build && \
 cp now.json build && \
 cd build && \
-now --prod --token $NOW_TOKEN && \
+now --prod --token $NOW_TOKEN --yes && \
 cd ../../../packages/lib && \
 yarn styleguide:build && \
 cp now.json styleguide && \
 cd styleguide && \
-now --prod --token $NOW_TOKEN
+now --prod --token $NOW_TOKEN --yes
